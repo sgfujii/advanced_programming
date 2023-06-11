47b916a40b7f060c1bb92274afd408abe12b41f8
Sogo Fujii <cmt14112@ict.nitech.ac.jp>
2023-06-11 18:28:27 +0900

    image processing changed.

--- advanced_programming_signal_processing/run.sh
+++ advanced_programming_signal_processing/run.sh
@@ -6,9 +6,9 @@ for image in $1/test/*.ppm; do
     name="imgproc/"$bname
     x=0    	#
     echo $name
-    convert "${image}" "${name}"  # 何もしない画像処理
+#   convert "${image}" "${name}"  # 何もしない画像処理
 #   convert -blur 2x6 "${image}" "${name}"
-#   convert -median 3 "${image}" "${name}"
+    convert -median 3 "${image}" "${name}"
 #   convert -auto-level "${image}" "${name}"
 #   convert -equalize "${image}" "${name}"
     rotation=0
